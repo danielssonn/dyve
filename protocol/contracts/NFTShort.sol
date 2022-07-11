b4a6bec7f163a0a5e530fb3e305a49bf02142c72
danielssonn <123456kolokol>
2022-07-11 17:51:03 -0400

    Development scaffold

---
+++ protocol/contracts/NFTShort.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/NFTUtility.sol";
+
+contract NFTShort is Ownable, NFTUtility {}
---
+++ protocol/contracts/interfaces/NFTUtility.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * Interface for Oracle clients to call from contracts and unit tests
+ */
+interface NFTUtility {
+
+}
