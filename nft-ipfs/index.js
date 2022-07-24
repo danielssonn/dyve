3ce74a7dcf95ba82208c69f40794b6dea87cd75f
danielssonn <123456kolokol>
2022-07-23 20:20:32 -0400

    Metadata match

--- nft-ipfs/index.js
+++ nft-ipfs/index.js
@@ -9,11 +9,20 @@ let hasMatchingTrait = function (CID, trait_type, trait_value) {
     // Pull the JSON metadata from IPFS, and let's assess it
     ipfs.files.get(CID, function (err, files) {
         files.forEach((file) => {
+
+            let metaData = JSON.parse(file.content.toString('utf8'));
+
+            for (let i = 0; i < metaData.attributes.length; i++) {
+                metaData.attributes[i].trait_type == trait_type && metaData.attributes[i].value == trait_value
+                console.log("Match!")
+                return true;
+            }
+            return false
             // do some fancy matching to see if the same or similar trait_type and trait_value is present in CID
-            return true;
+
         })
+
     })
-    return true;
 }
 exports.hasMatchingTrait = hasMatchingTrait;
 
